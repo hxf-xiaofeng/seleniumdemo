5d2c52774f55d76131f1fe07a8eff642c0751453
xiaofeng <379506993@qq.com>
2019-09-13 22:42:08 +0800

    update UseBrowser.java

--- src/test/java/CommonObject/UseBrowser.java
+++ src/test/java/CommonObject/UseBrowser.java
@@ -10,7 +10,7 @@ public class UseBrowser {
 
     //启动谷歌浏览器
     public WebDriver setupChrome(String test_url){
-        System.setProperty("webdriver.chrome.driver","E:\\Develop\\IntelliJ\\java_selenium\\src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","./src/main/resources/chromedriver.exe");//启动谷歌浏览器的路径使用相对路径，使用绝对路径提交到jenkins执行时会报错无法找到chromedriver.exe
         driver = new ChromeDriver();
         driver.get(test_url);
         driver.manage().window().maximize();
